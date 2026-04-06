34a081b25704043c3eb938de953d711b1771993c
GwenCodes <essexdesigns43.0@gmail.com>
2026-04-06 18:21:40 -0400

    Added author component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.html", // root-level HTML files
-    "./src/**/*.{html,js}", // article pages, styles, JS modules
+    "./*.html", // root-level HTML files (index.html, article.html)
+    "./components/*.html", // your new reusable components
+    "./css/**/*.css", // any custom CSS files
+    "./src/**/*.{html,js}", // article pages, scripts, or nested HTML
   ],
   theme: {
     extend: {},
